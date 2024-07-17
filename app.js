9ab0c632d43958ffc463fa0c404226442f5aa5d3
Tamerlan Mustafayev <tamerlan.m.2000@gmail.com>
2024-07-17 19:50:58 -0400

    Exercise 7 through 11

--- app.js
+++ app.js
@@ -50,13 +50,13 @@ Solve Exercise 4 here:
 const starterPokemonsArray = [] // Exercise is asking me to pick one starter Pokemon, so I made this array to store them all then pick one from
 
 pokemon.forEach(starterPokemon => {
-  if (starterPokemon.starter) {
-    // console.log(starterPokemon)
+  if (starterPokemon.starter === true) {
     starterPokemonsArray.push(starterPokemon) 
+    // console.log(starterPokemon)
   } 
 })
 
-game.party.push(starterPokemonsArray[0]) // 0 because I am only picking one pokemon that is starter
+game.party.push(starterPokemonsArray[0]) // 0a because I am only picking one pokemon that is starter
 // console.log(game.party)
 
 
@@ -70,11 +70,9 @@ Exercise 5
 Solve Exercise 5 here:
 */
 
-game.party.push(pokemon[1], pokemon[2])
+game.party.push(pokemon[1], pokemon[3], pokemon[44]) // I did not understand the instructions clearly here
 
-console.log(game.party)
-
-
+// console.log(game.party) 
 
 
 
@@ -91,6 +89,135 @@ game.gyms.forEach((city) => {
   if (city.difficulty < 3) {
     city.completed = true
   }
-}) 
+})
+
+/*
+Exercise 7
+1. Evolve the starter Pokémon you added to your party earlier. Each starter Pokémon evolves into a specific one.
+2. How would you replace the current starter Pokémon in your party with its evolved form?
+
+Hint: 
+  - Pokemon 1: Bulbasaur evolves into Pokemon 2: Ivysaur
+  - Pokemon 4: Charmander evolves into Pokemon 5: Charmeleon
+  - Pokemon 7: Squirtle evolves into Pokemon 8: Wartortle
+  - Pokemon 25: Pikachu evolves into Pokemon 26: Raichu 
+
+More Hints: The existing starter Pokemon will be *replaced* in your party with the Pokemon it evolved into. When working with an array of objects, the splice() array method is ideal for replacing one element with another. 
+
+Solve Exercise 7 here: 
+*/
+
+// console.log(game.party)
+
+// const unevolvedPokemonsArray =[] // array to store starter Pokemons
+// game.party.forEach(el => { // code block to push starter Pokemons from party array to unevolvedPokemonsArray array
+//   if (el.starter === true) {
+//     unevolvedPokemonsArray.push(el)
+//   }
+// })
+
+// // console.log(unevolvedPokemonsArray)
+
+// // this below is to get the pokemon which at 1 number higher position in the pokemon array compared to its unevolved pokemon version
+
+// pokemon[unevolvedPokemonsArray[0].number] // i did 0 and 1 because I know there is only two unevolved Pokemons in the array. I could do for loop too but too much work
+// pokemon[unevolvedPokemonsArray[1].number]
+
+
+// // code below is to store the indices of pokemons that needs to be evolved so that those indices can be used in the splice method later on
+// for (let i = 0; i < game.party.length; i++) {
+//   if (game.party[i].starter) {
+//     let indexOfPokemonsToBeEvolved = []
+//     indexOfPokemonsToBeEvolved.push(i)
+//     // console.log(indexOfPokemonsToBeEvolved)
+//   }
+// }
+
+// game.party.splice(0, 2, unevolvedPokemonsArray )
+
+
+
+
+
+
+
+
+
+
+/*
+Exercise 8
+1. Print the name of each Pokémon in your party.
+2. Consider using a loop or an array method to access each Pokémon's name.
+
+Solve Exercise 8 here:
+*/
+game.party.forEach(eachPokemon=> {
+  console.log(eachPokemon.name)
+})
+
+
+/*
+Exercise 9
+1. Can you print out all the starter Pokémon from the `pokemon` array?
+2. Think about how you can identify a starter Pokémon and then log their names.
+
+
+Solve Exercise 9 here:
+*/
+
+for (everyPokemon of pokemon) {
+  if (everyPokemon.starter) {
+    console.log(everyPokemon)
+  }
+}
+
+/*
+Exercise 10
+Create a method called `catchPokemon` and add it to the `game` object. You should not need to edit the original game object directly. This method should:
+  - Accept an object as a parameter called `pokemonObj`
+  - Add the `pokemonObj` to the `game.party` array.
+  - not return anything
+
+After writing this method, call it and pass in a Pokemon object of your choice from the `pokemon` data to catch it.
+
+Solve Exercise 10 here:
+
+*/
+
+game.catchPokemon = (pokemonObj) => {
+  game.party.push(pokemonObj)
+}
+
+
+/*
+Exercise 11
+1. Copy the `catchPokemon` method that you just wrote above, and paste it below. Modify it so that it also decreases the number of pokeballs in your inventory each time you catch a Pokémon.
+2. How will you find and update the quantity of pokeballs in the `game.items` array?
+
+Tips:
+For this exercise, it's okay to have a negative number of pokeballs.
+After updating the method, call it and pass in a Pokemon object of your choice from the `pokemon` data to catch it.
+Also, log the `game.items` array to confirm that the pokeball quantity is being decremented.
+
+Solve Exercise 11 here:
+*/
+game.catchPokemon = (pokemonObj) => {
+  game.party.push(pokemonObj)
+  game.items.forEach(item => {
+    if (item.name === 'pokeball') {
+      item.quantity -= 1
+    }
+  })
+}
+
+
+game.catchPokemon(pokemon[88])
+console.log(game.party, game.items)
+
+
+
+
+
+
 
 
